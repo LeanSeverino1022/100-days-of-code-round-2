9177c93af378b8d9714927a69e71f72e6147ecb0
Puyih Severino <yihseverino@gmail.com>
2019-10-17 18:37:54 +0800

    add vue proj - created via cli

---
+++ works/learning-vue/exercise-8/Section Code (Start)/src/components/UserEdit.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="component">
+        <h3>You may edit the User here</h3>
+        <p>Edit me!</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="editAge">Edit Age</button>
+    </div>
+</template>
+
+<script>
+
+    import { eventBus } from '../main';
+
+    export default {
+        props: {
+            userAge: Number,
+            editAgeFn: Function
+        },
+        methods: {
+            editAge() {
+                this.userAge = 50; /* should I just delete this? */
+
+                //commented line below to try out using event bus
+                // this.$emit( "ageWasEdited", this.userAge );
+
+                //communicate with userdetail directly without needing to pass through parent
+                eventBus.$emit('ageWasEdited', this.userAge);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightgreen;
+    }
+</style>
