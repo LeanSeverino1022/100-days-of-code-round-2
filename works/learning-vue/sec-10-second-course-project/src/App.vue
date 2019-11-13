1844641d36e2699c78f161eff2fda7b9e5e2b6d8
Puyih Severino <yihseverino@gmail.com>
2019-11-13 16:16:33 +0800

    section 10 course proj from start to finish

---
+++ works/learning-vue/sec-10-second-course-project/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote">        
+        </app-new-quote>
+        <app-quote-grid @quoteDeleted="deleteQuote" v-bind:quotes="quotes">
+
+        </app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to Delete it.</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import Header from './components/Header.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import QuoteGrid from './components/QuoteGrid.vue';
+
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    "Just a sample quote for now!",
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete some quotes first.');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {        
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ works/learning-vue/sec-10-second-course-project/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" 
+                :style="{ width:  quoteCount/maxQuotes * 100 + '%'}">
+                    <span>{{ quoteCount }} / {{ maxQuotes }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        props: {
+            quoteCount: Number,
+            maxQuotes: Number
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .row {
+        margin-top: 20px;
+    }
+ 
+</style>
